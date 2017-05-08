42dd90eef5c4fb50435529e724ef4d7b64b8ef1c
Alex Kostin <mlkostin256@gmail.com>
2017-05-08 03:47:05 +0300

    Load tests added. Constructor extension forbig amount of words created.

---
+++ LanguageGenerator.Core/Constructor/ConstructorExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace LanguageGenerator.Core.Constructor
+{
+    public static class ConstructorExtensions
+    {
+        public static int MaxAmountOfStringToExecuteInSingleThread = 10000;
+        public static List<string> GetStringListOfProprety(this ISyntacticUnitConstructor constructor, int amountOfStrings)
+        {
+            if (amountOfStrings < MaxAmountOfStringToExecuteInSingleThread)
+            {
+                return SingleThreadLoop(constructor, amountOfStrings);
+            }
+            return ParallelLoop(constructor, amountOfStrings);
+        }
+
+
+        private static List<string> SingleThreadLoop(ISyntacticUnitConstructor constructor, int amountOfWords)
+        {
+            List<string> words = new List<string>();
+            for (int i = 0; i < amountOfWords; i++)
+            {
+                string currentResult = constructor.GetResultStringOfProperty("ThreeLetters");
+                words.Add(currentResult);
+            }
+            return words;
+        }
+
+
+        private static List<string> ParallelLoop(ISyntacticUnitConstructor constructor, int amountOfWords)
+        {
+            List<string> words = new List<string>();
+            int degreeOfParallelism = Environment.ProcessorCount;
+            Task[] tasks = new Task[degreeOfParallelism];
+            int amountOfWordsOnOneThread = amountOfWords / degreeOfParallelism;
+            for (int taskNumber = 0; taskNumber < degreeOfParallelism; taskNumber++)
+            {
+                tasks[taskNumber] = Task.Factory.StartNew(
+                    () =>
+                    {
+                        for (int i = 0; i < amountOfWordsOnOneThread; i++)
+                        {
+                            string currentResult = constructor.GetResultStringOfProperty("ThreeLetters");
+                            words.Add(currentResult);
+                        }
+                    });
+            }
+
+            Task.WaitAll(tasks);
+            return words;
+        }
+    }
+}
---
+++ LanguageGenerator.Tests/Load_Tests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using LanguageGenerator.Core.AbstractFactory;
+using LanguageGenerator.Core.Constructor;
+using LanguageGenerator.Core.SyntacticProperty;
+using LanguageGenerator.Core.SyntacticUnit.ParentSU;
+using NUnit.Framework;
+
+
+namespace LanguageGenerator.Tests
+{
+    [TestFixture]
+    public class Load_Tests
+    {
+        public Load_Tests()
+        {
+            _consonants = new List<char> {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
+            _vowels = new List<char> {'a', 'e', 'i', 'o', 'u'};
+        }
+
+
+        private readonly List<char> _consonants;
+        private readonly List<char> _vowels;
+
+        private int amountOfWords = 10000;
+
+        private ILanguageFactory CreateRepositoryWithConsonantsAndVowels()
+        {
+            ILanguageFactory languageFactory = new LanguageFactory();
+            List<char> consonants = _consonants;
+            List<char> vowels = _vowels;
+            languageFactory.CreateRootProperty("consonant");
+            foreach (char c in consonants)
+            {
+                languageFactory.CreateRootSyntacticUnit(c.ToString(), "consonant");
+            }
+            languageFactory.CreateRootProperty("vowel");
+            foreach (char c in vowels)
+            {
+                languageFactory.CreateRootSyntacticUnit(c.ToString(), "vowel");
+            }
+
+            return languageFactory;
+        }
+
+
+        private SyntacticUnitConstructor CreateConstructorWithDefaultScheme()
+        {
+            ILanguageFactory languageFactory = CreateRepositoryWithConsonantsAndVowels();
+            SyntacticUnitConstructor constructor = new SyntacticUnitConstructor(languageFactory.Repository);
+            SetUpDefaultScheme(languageFactory, constructor);
+            return constructor;
+        }
+
+
+        private static void SetUpDefaultScheme(ILanguageFactory languageFactory, SyntacticUnitConstructor constructor)
+        {
+            languageFactory.Repository.GetRootPropertyWithName("consonant").CanStartFrom("Start").CanStartFrom("consonant").CanStartFrom("vowel");
+            languageFactory.Repository.GetRootPropertyWithName("vowel").CanStartFrom("Start").CanStartFrom("consonant").CanStartFrom("vowel");
+            languageFactory.CreateParentProperty("ThreeLetters").CanStartFrom("Start");
+            languageFactory.CreateParentSyntacticUnit("ThreeLetters").AddChildrenAmount(3, 1).AddPossibleChild("consonant").AddPossibleChild("vowel");
+            constructor.LinkRepository();
+        }
+
+
+        [Test]
+        public void Parralell_Attempt()
+        {
+            SyntacticUnitConstructor constructor = CreateConstructorWithDefaultScheme();
+            List<string> words = new List<string>();
+            int totalAmountOfWords = amountOfWords;
+            //Act
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            int degreeOfParallelism = Environment.ProcessorCount;
+            Task[] tasks = new Task[degreeOfParallelism];
+            int amountOfWordsOnOneThread = totalAmountOfWords / degreeOfParallelism;
+            for (int taskNumber = 0; taskNumber < degreeOfParallelism; taskNumber++)
+            {
+                tasks[taskNumber] = Task.Factory.StartNew(
+                    () =>
+                    {
+                        for (int i = 0; i < amountOfWordsOnOneThread;
+                             i++)
+                        {
+                            string currentResult = constructor.GetResultStringOfProperty("ThreeLetters");
+                            words.Add(currentResult);
+                        }
+                    });
+            }
+
+            Task.WaitAll(tasks);
+
+            sw.Stop();
+
+            Console.WriteLine("Miliseconds: " + sw.ElapsedMilliseconds);
+            Console.WriteLine("List length: " + words.Count);
+            //Assert
+        }
+
+
+        [Test]
+        public void Single_Thread()
+        {
+            SyntacticUnitConstructor constructor = CreateConstructorWithDefaultScheme();
+            List<string> words = new List<string>();
+            int totalAmountOfWords = amountOfWords;
+            //Act
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+
+            for (int i = 0; i < totalAmountOfWords; i++)
+            {
+                string currentResult = constructor.GetResultStringOfProperty("ThreeLetters");
+                words.Add(currentResult);
+            }
+
+
+            sw.Stop();
+
+            Console.WriteLine("Miliseconds: " + sw.ElapsedMilliseconds);
+            Console.WriteLine("List length: " + words.Count);
+        }
+    }
+}
